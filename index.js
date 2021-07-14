f064e41de6a6b054cda24b061190761465780bdc
Gururaj Patel <gururaj.p@simformsolutions.com>
2021-07-14 15:40:26 +0530

    refactor : app code

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-// import App from './App';
-import App from '../Storybook_demo/storybook/index';
-import {name as appName} from './app.json';
+// import {AppRegistry} from 'react-native';
+// // import App from './App';
+// import App from '../Storybook_demo/storybook/index';
+// import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+// AppRegistry.registerComponent(appName, () => App);
+
+export * from './src/index';
